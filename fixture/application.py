1b564aa611ae342ef82daefa7b13a7caee3a4a02
nlkazakova <45206433+nlkazakova@users.noreply.github.com>
2022-07-28 23:47:51 +0300

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -10,24 +11,11 @@ class Application:
         self.driver = webdriver.Firefox()
         self.vars = {}
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         self.driver.get("http://localhost/addressbook/")
 
-
-    def create_group(self, group):
-        self.driver.find_element(By.LINK_TEXT, "groups").click()
-        # init group creation
-        self.driver.find_element(By.NAME, "new").click()
-        # fill group form
-        self.driver.find_element(By.NAME, "group_name").send_keys("%s" % group.name)
-        self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys("%s" % group.header)
-        self.driver.find_element(By.NAME, "group_footer").click()
-        self.driver.find_element(By.NAME, "group_footer").send_keys("%s" % group.footer)
-        # submit group creation
-        self.driver.find_element(By.NAME, "submit").click()
-        self.driver.find_element(By.LINK_TEXT, "group page").click()
 
     def destroy(self):
         self.driver.quit()
---
+++ fixture/group.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_groups_page(self):
+        self.app.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def create(self, group):
+        self.open_groups_page();
+        self.app.driver.find_element(By.LINK_TEXT, "groups").click()
+        # init group creation
+        self.app.driver.find_element(By.NAME, "new").click()
+        # fill group form
+        self.app.driver.find_element(By.NAME, "group_name").send_keys("%s" % group.name)
+        self.app.driver.find_element(By.NAME, "group_header").click()
+        self.app.driver.find_element(By.NAME, "group_header").send_keys("%s" % group.header)
+        self.app.driver.find_element(By.NAME, "group_footer").click()
+        self.app.driver.find_element(By.NAME, "group_footer").send_keys("%s" % group.footer)
+        # submit group creation
+        self.app.driver.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        self.app.driver.find_element(By.LINK_TEXT, "group page").click()
