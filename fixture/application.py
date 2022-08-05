b500fd94beb590946763910af366c59260c819ab
nlkazakova <45206433+nlkazakova@users.noreply.github.com>
2022-08-06 00:52:36 +0300

    Добавлены параметры командной строки для запуска в разных браузерах

--- fixture/application.py
+++ fixture/application.py
@@ -7,14 +7,22 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
+        self.driver.get(self.base_url)
 
     def is_valid(self):
         try:
