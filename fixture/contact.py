5599f732769fdd298c6d1df452d6a5646cbae9a6
nlkazakova <45206433+nlkazakova@users.noreply.github.com>
2022-08-05 22:16:58 +0300

    Выполнено более точное извлечение данных с помощью join, map, filter, lambda

--- fixture/contact.py
+++ fixture/contact.py
@@ -26,10 +26,9 @@ class ContactHelper:
                 firstname = cells[1].text
                 lastname = cells[2].text
                 id = cells[0].find_element(By.TAG_NAME, "input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
+                all_phones = cells[5].text
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
-                                                  homephone=all_phones[0], mobilephone=all_phones[1],
-                                                  workphone=all_phones[2], secondaryphone=all_phones[3]))
+                                                  all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -54,8 +53,8 @@ class ContactHelper:
         workphone = self.app.driver.find_element(By.NAME, "work").get_attribute("value")
         secondaryphone = self.app.driver.find_element(By.NAME, "phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id,
-                       homephone=homephone, workphone=workphone,
-                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+                       homephone=homephone, mobilephone=mobilephone, workphone=workphone,
+                       secondaryphone=secondaryphone)
 
     def get_contact_from_view_page(self, index):
         self.open_contact_view_by_index(index)
