7654e4b50ad997e93f08c1879e5a8571b4607e2d
nlkazakova <45206433+nlkazakova@users.noreply.github.com>
2022-08-07 09:33:23 +0300

    Revert "Реализовано чтение параметров из файла (6.2)"
    This reverts commit 1049a65dd95c1dc2eea9ee2e43f05c87710eaa7d.

--- conftest.py
+++ conftest.py
@@ -1,23 +1,21 @@
-import json
 import pytest
-import os.path
 from fixture.application import Application
 
 fixture = None
-target = None
+
 
 @pytest.fixture()
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target['baseUrl'])
-    fixture.session.login(username=target['username'], password=target['password'])
+    base_url = request.config.getoption("--baseUrl")
+    if fixture is None:
+        fixture = Application(browser=browser, base_url=base_url)
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=base_url)
+            fixture.session.login(username="admin", password="secret")
     return fixture
 
 
@@ -31,4 +29,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
