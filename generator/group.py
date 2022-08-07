259b73bf51f6946f1220576b71db904f020f55f6
nlkazakova <45206433+nlkazakova@users.noreply.github.com>
2022-08-07 09:32:31 +0300

    Revert "Добавлена генерация данных в файл с чтением параметров из конфигурации (6.4)"
    This reverts commit 4c6a84bd95363f32a4c0b4f11b2ed854804a7656.

--- generator/group.py
+++ generator/group.py
@@ -1,26 +1,8 @@
-from model.group import Group
 import os.path
 import random
 import string
 import json
-import getopt
-import sys
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
-except getopt.GetoptError as err:
-    print(err)
-    getopt.usage()
-    sys.exit(2)
-
-n = 5
-f = "data/groups.json"
-
-for o, a in opts:
-    if o == "-n":
-        n = int(a)
-    elif o == "-f":
-        f = a
+from model.group import Group
 
 
 def random_string(prefix, maxlen):
@@ -28,11 +10,13 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(n)
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
-with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+with open(file, "w") as f:
+    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
